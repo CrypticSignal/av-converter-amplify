3ba95c2d87479e3671943d53f4441da02dd4c450
Hamas Shafiq <hs@bluetel.co.uk>
2023-10-08 18:31:57 +0100

    Add headers to prevent SharedArrayBuffer is not defined

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config, options) => {
+    config.devtool = false;
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
