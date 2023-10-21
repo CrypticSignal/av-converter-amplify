69f8db1ab61326a326f4298cec4c314b61a3c698
Hamas Shafiq <hs@bluetel.co.uk>
2023-10-21 17:40:22 +0100

    test

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: "/_next/static/chunks",
         headers: [
           {
             key: "Cross-Origin-Embedder-Policy",
